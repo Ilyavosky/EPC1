d1dfe0a2fda92d2ff9efda03e3c286a90b4c1ee9
Ilyavosky <ilyacortesruiz@gmail.com>
2026-02-04 18:42:04 -0600

    Asignar rol para el uso de la Db

---
+++ db/05_roles.sql
@@ -0,0 +1,21 @@
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'app_user') THEN
+      CREATE ROLE app_user WITH LOGIN PASSWORD 'app_secure_password_2026';
+   END IF;
+END
+$do$;
+
+GRANT CONNECT ON DATABASE ep_db TO app_user;
+GRANT USAGE ON SCHEMA public TO app_user;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM app_user;
+
+GRANT SELECT ON vw_most_borrowed_books TO app_user;
+GRANT SELECT ON vw_overdue_loans TO app_user;
+GRANT SELECT ON vw_fines_summary TO app_user;
+GRANT SELECT ON vw_member_activity TO app_user;
+GRANT SELECT ON vw_inventory_health TO app_user;
